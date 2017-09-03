a93dccbf77fec45ba15a5aa2785dc200e7e7121e
mattkirchner <mattkirchner@10.30.20.242>
2017-09-03 14:37:42 -0500

    Added arraylist and constructor to lab 4

---
+++ src/lab4/HrEmployee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+import java.util.*;
+
+/**
+ *
+ * @author mattkirchner
+ */
+public class HrEmployee {
+    private List<Employee> listOfEmployees;
+
+    public HrEmployee() {
+    }
+    
+    public void hireEmployee(String firstName, String lastName, String ssn) {
+        Employee e = new Employee(firstName,lastName,ssn);
+        listOfEmployees.add(e);
+        orientNewEmployee(e);
+    }
+    
+    public void orientNewEmployee(Employee e) {
+        e.doFirstTimeOrientation("A7");
+    }
+}
